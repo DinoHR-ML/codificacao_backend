78466b6a03b19c815c8a0d9813d8d0097906dc02
Henrique <henrique.hoppen@gmail.com>
2026-03-18 16:45:11 -0300

    Update server.js

--- 18-03-2026/atv-config-server-route-service/src/server.js
+++ 18-03-2026/atv-config-server-route-service/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import studentRoutes from './routes/studentRoutes.js'
+import route from './routes/studentRoutes.js'
 const app = express()
 app.use(express.json())
 const port = 3000
@@ -8,7 +9,7 @@ app.get("/", (req, res) => {
     res.json("Olá, mundo!")
 })
 
-app.use("/students", studentRoutes)
+app.use("/students", route)
 
 app.listen(port, () => {
     console.log(`O servidor está sendo rodado na porta: http://localhost:${port}`)
